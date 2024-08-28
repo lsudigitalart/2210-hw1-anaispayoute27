5dedf41531b814491b1511dd36e9c3d3e097475d
Anais Payoute <apayou1 @lsu.edu>
2024-08-28 14:50:48 -0500

    made a thing

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+ createCanvas(windowHeight , windowWidth);
+ background(0);
+ ellipse(width/2, height/2, 50, 50)
+ fill(234, 90, 43)
+ ellipse(width/2, 2, 50, 50)
+ fill(140, 30, 90)
+ ellipse(width/2, height/2, 50, 50)
+ stroke(0, 0, 0)
+ fill(100)
+}
+
+function draw() {
+
+}
